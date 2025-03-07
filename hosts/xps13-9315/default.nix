45a3a6bb3e49ecd178c2156ac1ac2016f2b539c4
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:12:08 +0100

    🏗️added keyring pkgs to user scope

--- hosts/xps13-9315/default.nix
+++ hosts/xps13-9315/default.nix
@@ -10,6 +10,7 @@
   users.users.sebastian.packages = with pkgs; [
     vscode
     zen-browser.packages."x86_64-linux".default
+    libgnome-keyring
   ];
 
   environment.systemPackages = with pkgs; [

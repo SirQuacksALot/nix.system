3247260829b6b54c1e4f124b7095d13e997111a1
Sebastian <sglass@hs-mittweida.de>
2025-03-07 21:59:30 +0100

    🏗️added dsiplaymanager prefix to default session

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -3,7 +3,7 @@
 {
   # use gnome as display backend
   services.xserver.enable = true;
-  services.xserver.defaultSession = "hyprland";
+  services.xserver.displayManager.defaultSession = "hyprland";
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;

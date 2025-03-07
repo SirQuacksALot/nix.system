7ca0fb14165faa620f7fc3205a63017f119dc964
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:14:28 +0100

    🏗️removed keyring pkgs

--- hosts/xps13-9315/default.nix
+++ hosts/xps13-9315/default.nix
@@ -10,7 +10,6 @@
   users.users.sebastian.packages = with pkgs; [
     vscode
     zen-browser.packages."x86_64-linux".default
-    libgnome-keyring
   ];
 
   environment.systemPackages = with pkgs; [

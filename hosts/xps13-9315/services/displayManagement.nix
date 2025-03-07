297ffa85470349c63d9177fdab8604cad437a064
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:39:34 +0100

    🏗️added polkit

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [ libsecret ];
+
   # use gnome as display backend
   services.xserver.enable = true;
   services.xserver.displayManager.defaultSession = "hyprland";
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  services.gnome.gnome-keyring.enable = true;
+  security.polkit.enable = true; 
 
   xdg.portal = {
     enable = true;

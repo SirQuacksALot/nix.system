ce3c5ea4ea7dbc6b4b3e65e5397cde5f469067f8
Sebastian <sglass@hs-mittweida.de>
2025-03-07 17:50:28 +0100

    🚧debug test with setting pkgs

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
 
   outputs = { self, nixpkgs }@attrs: {
     nixosConfigurations.xps13-9315 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      pkgs = nixpkgs {
+        config.allowUnfree = true;
+      };
       modules = [
         ./nixos/configuration.nix
       ];

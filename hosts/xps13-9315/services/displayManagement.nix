913c0d75832595744ee14b4b5d4e433d529f399c
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:18:00 +0100

    😺added portals

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # use gnome as display backend
@@ -7,4 +7,13 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extaPortals = {
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gnome
+    }
+  }
 }

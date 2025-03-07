48893b88426b1eb83d795fe923f2a60ab23b6786
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:09:58 +0100

    🏗️moved vscode and zen browser pkgs to user pkgs

--- hosts/xps13-9315/default.nix
+++ hosts/xps13-9315/default.nix
@@ -7,9 +7,12 @@
     ./programs
   ];
 
-  environment.systemPackages = with pkgs; [
+  users.users.sebastian.packages = with pkgs; [
     vscode
     zen-browser.packages."x86_64-linux".default
+  ];
+
+  environment.systemPackages = with pkgs; [
   ];
 
 }

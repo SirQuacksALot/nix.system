f4ff718b28f0c63ab6c0349dc7534b05ab3c0d17
Sebastian <sglass@hs-mittweida.de>
2025-03-07 19:10:13 +0100

    😺added default user module

---
+++ hosts/xps13-9315/modules/default-user.nix
@@ -0,0 +1,40 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.default-user;
+in
+{
+  options.default-user = {
+    enable = lib.mkEnableOption "enable user module";
+    username = lib.mkOption {
+      default = "sebastian";
+      description = "username";
+    };
+  };
+  
+  config = lib.mkIf cfg.enable{
+    # Enable zsh - used as default shell
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      ohMyZsh.enable = true;
+      ohMyZsh.plugins = [ "git" "sudo" "docker" "kubectl" ];
+      ohMyZsh.theme = "robbyrussell";
+      syntaxHighlighting.enable = true;
+    };
+    
+    # create user
+    users.groups.nixos-manage = {};
+    users.users.${cfg.username} = {
+      isNormalUser = true;
+      initialPassword = "123";
+      description = "${cfg.username}";
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" "docker" "nixos-manage" ];
+      home = "/home/${cfg.username}";
+      packages = with pkgs; [
+        tree
+      ];
+    };
+  };
+}
--- hosts/xps13-9315/modules/default.nix
+++ hosts/xps13-9315/modules/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     # ...
+    ./default-user.nix
   ];
 }

d2f66fedd4155f9f8eeae67772f7a38a7eb4c6b5
Sebastian <sglass@hs-mittweida.de>
2025-03-07 20:08:52 +0100

    🏗️try to inherit inputs

--- flake.nix
+++ flake.nix
@@ -2,15 +2,19 @@
   description = "A very basic flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.zen-browser.url = "github:0xc000022070/zen-browser-flake";
   inputs.nixos-hardware.url = "github:Nixos/nixos-hardware/master";
+  inputs.zen-browser.url = "github:MarceColl/zen-browser-flake";
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@attrs: {
+  outputs = { self, nixpkgs, nixos-hardware, inputs, ... }@attrs: {
+
     nixosConfigurations.xps13-9315 = nixpkgs.lib.nixosSystem {
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
+
+      __module.args = { inherit inputs; };
+
       modules = [
         nixos-hardware.nixosModules.dell-xps-13-9315
         ./nixos/configuration.nix

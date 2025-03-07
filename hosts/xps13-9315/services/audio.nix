06e23ac66eedd9807c1e7f652ac83622fd0f4e3a
Sebastian <sglass@hs-mittweida.de>
2025-03-07 23:50:09 +0100

    🏗️added more audio and bluetooth options

--- hosts/xps13-9315/services/audio.nix
+++ hosts/xps13-9315/services/audio.nix
@@ -2,5 +2,32 @@
 
 {
   services.pipewire.enable = lib.mkForce false;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+    configFile = pkgs.writeText "default.pa" ''
+      load-module module-bluetooth-policy
+      load-module module-bluetooth-discover
+      ## module fails to load with 
+      ##   module-bluez5-device.c: Failed to get device path from module arguments
+      ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+      # load-module module-bluez5-device
+      # load-module module-bluez5-discover
+    '';
+    
+  };
+
+  hardware.bluetooth.settings = {
+    General = {
+		  Experimental = true;
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  }; 
 }

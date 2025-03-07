b79875517b52882ca1bdc4a8dd430e3e7605a306
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:45:49 +0100

    🏗️added more polkit related stuff

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -13,7 +13,25 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.hyprland.enableGnomeKeyring = true;
   security.polkit.enable = true; 
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = “polkit-gnome-authentication-agent-1”;
+      wantedBy = [ “graphical-session.target” ];
+      wants = [ “graphical-session.target” ];
+      after = [ “graphical-session.target” ];
+      serviceConfig = {
+      Type = “simple”;
+      ExecStart = “${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1”;
+      Restart = “on-failure”;
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 
   xdg.portal = {
     enable = true;

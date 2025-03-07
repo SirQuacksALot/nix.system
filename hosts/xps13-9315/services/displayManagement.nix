806f5a6feedd3bd9aef9837d634ea7ea6b3926af
Sebastian <sglass@hs-mittweida.de>
2025-03-07 22:23:28 +0100

    🏗️fixed misspelling

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -10,7 +10,7 @@
 
   xdg.portal = {
     enable = true;
-    extaPortals = [
+    extraPortals = [
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland

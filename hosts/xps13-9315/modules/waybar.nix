4b0dd508720637cd80602aa98664e14890ec50fc
Sebastian <sglass@hs-mittweida.de>
2025-03-08 03:42:34 +0100

    🏗️try to fix

--- hosts/xps13-9315/modules/waybar.nix
+++ hosts/xps13-9315/modules/waybar.nix
@@ -23,21 +23,40 @@ in
   options.waybar = with lib.types; {
     # Should it be enabled ? 
     enable = mkEnableOption "waybar";
+
     # Package option
     package = mkOption {
       type = package;
       default = pkgs.waybar;
       defaultText = literalExpression "pkgs.waybar";
     };
+
     # Config content as path or sting '' content ''
     settings = mkOption {
       type = nullOr (either path lines);
       default = null;
     };
+
     # Css styling as path ort css string '' content ''
     style = mkOption {
       type = nullOr (either path lines);
       default = null;
+    };
+
+    # Enable System service
+    systemd.enable = mkEnableOption "Waybar systemd integration";
+
+    # Define target fpr system service
+    systemd.target = mkOption {
+      type = nullOr str;
+      default = config.wayland.systemd.target;
+      defaultText = literalExpression "config.wayland.systemd.target";
+    };
+    
+    # Enable to inspect obejcts - docs: https://developer.gnome.org/documentation/tools/inspector.html
+    systemd.enableInspect = mkOption {
+      type = bool;
+      default = false;
     };
   };
 
@@ -48,25 +67,55 @@ in
     ];
 
     # make config file
-    xdg.configFile."waybar/config" = mkIf (cfg.settings != null){
+    configFile = mkIf (cfg.settings != null){
       source = if builtins.isPath cfg.settings || isStorePath cfg.settings then
         cfg.settings
       else 
-        pkgs.writeText "waybar/config" cfg.settings;
+        pkgs.writeTextDir "~/.config/waybar/config" cfg.settings;
       onChange = ''
         ${pkgs.procps}/bin/pkill -u $USER -USR2 waybar || true
       '';
     };
     
     # make style file
-    xdg.configFile."waybar/style.css" = mkIf (cfg.style != [ ]){
+    styleFile = mkIf (cfg.style != null){
       source = if builtins.isPath cfg.style || isStorePath cfg.style then
         cfg.style
       else 
-        pkgs.writeText "waybar/style.css" cfg.style;
+        pkgs.writeTextDir "~/.config/waybar/style.css" cfg.style;
       onChange = ''
         ${pkgs.procps}/bin/pkill -u $USER -USR2 waybar || true
       '';
     };
+
+    # Make a system service
+    (mkIF cfg.systemd.enable {
+      systemd.user.services.waybar = {
+        Unit = {
+          Description =
+            "Highly customizable Wayland bar for Sway and Wlroots based compositors.";
+          Documentation = "https://github.com/Alexays/Waybar/wiki";
+          PartOf = [ cfg.systemd.target ];
+          After = [ cfg.systemd.target ];
+          ConditionEnvironment = "WAYLAND_DISPLAY";
+          X-Restart-Triggers = optional (settings != null)
+            "${config.configFile.source}"
+            ++ optional (cfg.style != null)
+            "${config.styleFile.source}";
+        };
+
+        Service = {
+          ExecStart = "${cfg.package}/bin/waybar";
+          ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+          Restart = "on-failure";
+          KillMode = "mixed";
+        } // optionalAttrs cfg.systemd.enableInspect {
+          Environment = [ "GTK_DEBUG=interactive" ];
+        };
+
+        Install.WantedBy =
+          lib.optional (cfg.systemd.target != null) cfg.systemd.target;
+      };
+    })
   };
 }

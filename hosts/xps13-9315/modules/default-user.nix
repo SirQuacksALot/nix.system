aa1a05dc33fad1614f80dc44f5c0fda1555eff06
Sebastian <sglass@hs-mittweida.de>
2025-03-08 01:36:22 +0100

    🚧removed powerlevel10k from zsh

--- hosts/xps13-9315/modules/default-user.nix
+++ hosts/xps13-9315/modules/default-user.nix
@@ -21,18 +21,6 @@ in
       ohMyZsh.plugins = [ "git" "sudo" "docker" "kubectl" ];
       ohMyZsh.theme = "robbyrussell";
       syntaxHighlighting.enable = true;
-      plugins = [
-        {
-          name = "powerlevel10k-config";
-          src = ./p10k;
-          file = "p10k.zsh";
-        }
-        {
-          name = "zsh-powerlevel10k";
-          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-          file = "powerlevel10k.zsh-theme";
-        }
-      ];
     };
     
     # create user

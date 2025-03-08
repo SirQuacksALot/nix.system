f8fe4f68a81f902090d0db6591a6103463820a27
Sebastian Glass <sglass@hs-mittweida.de>
2025-03-08 18:07:38 +0100

    🏗️changed audio driver

--- hosts/xps13-9315/services/audio.nix
+++ hosts/xps13-9315/services/audio.nix
@@ -1,21 +1,7 @@
 { lib, pkgs, ... }:
 
 {
-  services.pipewire.enable = lib.mkForce false;
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
-    configFile = pkgs.writeText "default.pa" ''
-      load-module module-bluetooth-policy
-      load-module module-bluetooth-discover
-      ## module fails to load with 
-      ##   module-bluez5-device.c: Failed to get device path from module arguments
-      ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-      # load-module module-bluez5-device
-      # load-module module-bluez5-discover
-    '';
-    
-  };
+  services.pipewire.enable = true;
 
   hardware.bluetooth.settings = {
     General = {
@@ -30,4 +16,8 @@
     wantedBy = [ "default.target" ];
     serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   }; 
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
 }

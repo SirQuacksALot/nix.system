16b65bfc40c729d36bb18938425a3250a96fec10
Sebastian <sglass@hs-mittweida.de>
2025-03-08 06:51:14 +0100

    🏗️try fix

--- hosts/xps13-9315/modules/waybar.nix
+++ hosts/xps13-9315/modules/waybar.nix
@@ -56,6 +56,7 @@ in
     systemd.user.services.waybar = mkIf cfg.systemd.enable{
       enable = true;
       after = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
 
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/waybar";

294216ffaec4fdb0c449e105d416b9446bb0ac81
Sebastian Glass <sglass@hs-mittweida.de>
2025-03-09 05:36:54 +0100

    🏗️renamed to new

--- hosts/xps13-9315/services/displayManagement.nix
+++ hosts/xps13-9315/services/displayManagement.nix
@@ -15,7 +15,7 @@
 
   # use gnome as display backend
   services.xserver.enable = true;
-  services.xserver.displayManager.defaultSession = "hyprland-uwsm";
+  services.displayManager.defaultSession = "hyprland-uwsm";
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;

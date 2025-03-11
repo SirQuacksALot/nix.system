764eb91d222d9e018e3aaf338cab4f91a5009299
Sebastian <sglass@hs-mittweida.de>
2025-03-12 00:03:47 +0100

    returned to original way for config fiel

--- system-environment/desktop/hyprland/services/greetd/default.nix
+++ system-environment/desktop/hyprland/services/greetd/default.nix
@@ -1,17 +1,15 @@
 { pkgs, username, ... }: 
 let
-  inherit (import ../../conifg.nix) hyprconfig;
+  inherit (import ../../default.nix) hyprconfig;
 in
 {
-  configFile = pkgs.writeText "hyprland.conf" ${hyprconfig};
-
   services.greetd = {
     enable = true;
     vt = 3;
     settings = {
       default_session = {
         user = username;
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --config ${configFile}"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --config ${hyprconfig}"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
       };
     };
   };

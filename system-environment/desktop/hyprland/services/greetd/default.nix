9bb70a880830191fdfa36468c3594bae8a561e7a
Sebastian <sglass@hs-mittweida.de>
2025-03-11 23:57:31 +0100

    put config in its own file and write inside greeded config

--- system-environment/desktop/hyprland/services/greetd/default.nix
+++ system-environment/desktop/hyprland/services/greetd/default.nix
@@ -1,15 +1,17 @@
 { pkgs, username, ... }: 
 let
-  inherit (import ../../default.nix) hyprconfig;
+  inherit (import ../../conifg.nix) hyprconfig;
 in
 {
+  configFile = pkgs.writeText "hyprland.conf" ${hyprconfig};
+
   services.greetd = {
     enable = true;
     vt = 3;
     settings = {
       default_session = {
         user = username;
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --config ${hyprconfig}"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --config ${configFile}"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
       };
     };
   };

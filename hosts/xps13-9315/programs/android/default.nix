c112288c47862ddf095d118457c17d95885c5d08
Sebastian Glass <sglass@hs-mittweida.de>
2025-03-11 06:37:34 +0100

    updated System

--- hosts/xps13-9315/programs/android/default.nix
+++ hosts/xps13-9315/programs/android/default.nix
@@ -3,7 +3,25 @@
 {
   environment.systemPackages = with pkgs; [
     android-studio
+    android-tools
+    android-studio-tools
+    flutter
+    dart
     flutterPackages-source.stable
     jdk17
+    adb-sync
+    google-chrome
+  ];
+
+  environment.sessionVariables = {
+    CHROME_EXECUTABLE = pkgs.google-chrome.meta.mainProgram;
+    ANDROID_HOME = "${pkgs.android-tools}/bin";
+  };
+
+  programs.adb.enable = true;
+  users.users.sebastian.extraGroups = ["adbusers" "kvm"]; # kvm might not work - trys to help with hardware acceleration for emulator - docs related ref.: https://github.com/NixOS/nixpkgs/issues/41703
+
+  services.udev.packages = [
+    pkgs.android-udev-rules
   ];
 }

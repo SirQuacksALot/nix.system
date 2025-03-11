0328d99960f45bd69225c81b9229a0e8f52b8285
Sebastian <sglass@hs-mittweida.de>
2025-03-11 17:27:44 +0100

    switched bootlaoder to test to fix problems

--- system-environment/services/bootloader/default.nix
+++ system-environment/services/bootloader/default.nix
@@ -1,17 +1,19 @@
 { host, pkgs, inputs, ... }: {
   boot = {
-    loader.grub = {
-      enable = true;
-      devices = [ "nodev" ];
-      efiSupport = true;
-      gfxmodeBios = "auto";
-      memtest86.enable = true;
-      extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
-      configurationName = "${host}";
-      theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
-  	};
+    # loader.grub = {
+    #   enable = true;
+    #   devices = [ "nodev" ];
+    #   efiSupport = true;
+    #   gfxmodeBios = "auto";
+    #   memtest86.enable = true;
+    #   extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
+    #   configurationName = "${host}";
+    #   theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
+  	# };
 
-    plymouth.enable = true; # plymouth loading overlay - may cause issues with graphics driver loading and displayLink driver
+    # plymouth.enable = true; # plymouth loading overlay - may cause issues with graphics driver loading and displayLink driver
 
+    loader.systemd-boot-enable = true;
+    loader.efi.canTouchEfiVaribales = true;
   };
 }

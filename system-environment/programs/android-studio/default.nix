7825e14e56be607cbbb03d1f7745f691b070b9b5
Sebastian Glass <sglass@hs-mittweida.de>
2025-03-11 13:00:34 +0100

    added missing pkgs input

--- system-environment/programs/android-studio/default.nix
+++ system-environment/programs/android-studio/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   users.users.sebastian.packages = with pkgs; [
     android-studio
   ];
--- system-environment/programs/zen-browser/default.nix
+++ system-environment/programs/zen-browser/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   users.users.sebastian.packages = with pkgs; [
     inputs.zen-browser.packages."x86_64-linux".default
   ];

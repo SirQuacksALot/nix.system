4a29d07689dcd00c253540aa69d5bba8ed069834
Sebastian <sglass@hs-mittweida.de>
2025-03-11 23:19:55 +0100

    added specified config

--- system-environment/desktop/hyprland/services/greetd/default.nix
+++ system-environment/desktop/hyprland/services/greetd/default.nix
@@ -1,11 +1,15 @@
-{ pkgs, username, ... }: {
+{ pkgs, username, ... }: 
+let
+  inherit (import ../../../default.nix) config;
+in
+{
   services.greetd = {
     enable = true;
     vt = 3;
     settings = {
       default_session = {
         user = username;
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --config ${config}"; # start Hyprland with a TUI login manager - change to service run. so other programs can a dependent service too
       };
     };
   };

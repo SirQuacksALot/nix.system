4a1e8175704fc84f25c496e8be85e1e4e5a82491
Sebastian Glaß <sglass@hs-mittweida.de>
2025-03-15 14:56:13 +0100

    removed active icon

--- system-environment/desktop/hyprland/configs/waybar.nix
+++ system-environment/desktop/hyprland/configs/waybar.nix
@@ -150,7 +150,6 @@
                 "3": "󰍹 ",
                 "urgent": " ",
                 "focused": " ",
-                // "active": " ",
                 "default": " " 
             }
         },

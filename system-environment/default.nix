0f0c64bb192bb75c47e8bf8877949d41c7d90c57
Sebastian Glaß <sglass@hs-mittweida.de>
2025-03-19 06:07:49 +0100

    enabled flutter

--- system-environment/default.nix
+++ system-environment/default.nix
@@ -11,4 +11,12 @@
   # Define the system user
   system-user.enable = true;
   system-user.username = username;
+
+  # enable dev stuff - not final
+  programs.flutter = {
+    enable = true;
+    user = username; # Replace with your actual username
+    enableAdb = true;       # Enable ADB for Android debugging
+    addToKvmGroup = true;   # Add to KVM group for hardware acceleration
+  };
 }

4aa0abdc83a8d277fe4158e3fbdaf97969e786f9
Sebastian Glaß <sglass@hs-mittweida.de>
2025-04-02 16:34:04 +0200

    added ms teams

--- system-environment/packages/default.nix
+++ system-environment/packages/default.nix
@@ -11,21 +11,21 @@ in
 {
   programs.nix-ld.enable = true;
   environment.systemPackages = (with pkgs; [
-    clang         # build tools
-    curl          # transfaring files with url 
-    git           # version control
-    killall       # kill all related processes
-    nano          # editor
-    openssl       # cryptographic library
-    pciutils      # pci inspection and manipulation
-    wget          # retrive files via https, http and ftp
-    python314     # python programming language
-    fastfetch     # cli system info fetcher
-    # unstable.warp-terminal # currently bugged - wayland support does not work on this system dunno why ´\_(°.°)_/`
-    zoom-us       # video conference
+    clang           # build tools
+    curl            # transfaring files with url 
+    git             # version control
+    killall         # kill all related processes
+    nano            # editor
+    openssl         # cryptographic library
+    pciutils        # pci inspection and manipulation
+    wget            # retrive files via https, http and ftp
+    python314       # python programming language
+    fastfetch       # cli system info fetcher
+    zoom-us         # video conference
+    teams-for-linux # teams
     gradle
     act
-    eduvpn-client # HSMW vpn Client
+    eduvpn-client   # HSMW vpn Client
     php
   ]) ++  
   [ 

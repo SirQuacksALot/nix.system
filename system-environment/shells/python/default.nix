e59ecbcf716817375d8a6efe940357ae98b4978e
Sebastian Glaß <sglass@hs-mittweida.de>
2025-04-23 15:22:11 +0200

    added comment

--- system-environment/shells/python/default.nix
+++ system-environment/shells/python/default.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [
     (python3.withPackages(p: with p; [
+      # packages have to be in nix pkgs
       requests
       pyquery
       discordpy
